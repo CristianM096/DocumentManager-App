1806b4040c1b430ac3ce4aa3fe2d4d9a65249c31
Chriztian Manzano <cristianorb@unicauca.edu.co>
2023-01-07 08:21:16 -0500

    ini: Project Creation

---
+++ app/src/main/java/com/sophos/documentmanager/ui/login/ui/LoginViewModel.kt
@@ -0,0 +1,37 @@
+package com.sophos.documentmanager.ui.login.ui
+
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.util.Patterns
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
+import java.util.regex.Pattern
+
+class LoginViewModel:ViewModel(){
+    private val _email = MutableLiveData<String>()
+    val email : LiveData<String> = _email
+
+    private val _password = MutableLiveData<String>()
+    val password : LiveData<String> = _password
+
+    private val _loginEnable = MutableLiveData<Boolean>()
+    val loginEnable : LiveData<Boolean> = _loginEnable
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading : LiveData<Boolean> = _isLoading
+
+    fun onLoginChanged(email: String, password: String) {
+        _email.value = email
+        _password.value = password
+        _loginEnable.value = isValidEmail(email) && isValidPassoword(password)
+    }
+
+    private fun isValidPassoword(password: String): Boolean = password.length > 6
+    private fun isValidEmail(email: String):Boolean = Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    suspend fun onLoginSelected() {
+        _isLoading.value = true
+        delay(4000)
+        _isLoading.value = false
+    }
+}
